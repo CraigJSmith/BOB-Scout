6014508652dd4d5af80aae2b6b40fd419cfb6fcd
Craig Smith <craigjsmith99@gmail.com>
2018-02-22 18:21:37 -0500

    Warning when overwriting file

--- BOB Scout/src/main/java/com/example/craig/bobscout/ScoutSetup.java
+++ BOB Scout/src/main/java/com/example/craig/bobscout/ScoutSetup.java
@@ -1,15 +1,26 @@
 package com.example.craig.bobscout;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.File;
+import java.util.Arrays;
 
 public class ScoutSetup extends AppCompatActivity {
 
     private EditText matchNumBox;
     private EditText teamNumBox;
+    private String matchNum;
+    private String teamNum;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,11 +32,37 @@ public class ScoutSetup extends AppCompatActivity {
     }
 
     public void submit(View v) {
+        matchNum = matchNumBox.getText().toString();
+        teamNum = teamNumBox.getText().toString();
+
+        File dir = new File(Environment.getExternalStorageDirectory(), "/BOBScout");
+        File[] files = dir.listFiles();
+
+        if(Arrays.asList(files).contains(new File(Environment.getExternalStorageDirectory().toString() + "/BOBScout/" + matchNum + ":" + teamNum + ".csv"))) {
+            AlertDialog alertDialog = new AlertDialog.Builder(this).create();
+            alertDialog.setTitle("Overwrite match?");
+            alertDialog.setMessage("This match has already been scouted. Are you sure you want to redo it?");
+
+            alertDialog.setButton(Dialog.BUTTON_POSITIVE,"Yes, redo", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int which) {
+                    begin();
+                }
+            });
+
+            alertDialog.setButton(Dialog.BUTTON_NEGATIVE, "No", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int which) {
+                    // here you can add functions
+                }
+            });
+            alertDialog.show();
+        } else {
+            begin();
+        }
+
+    }
+
+    private void begin() {
         Intent intent = new Intent(this, ScoutMatch.class);
-
-        String matchNum = matchNumBox.getText().toString();
-        String teamNum = teamNumBox.getText().toString();
-
         Bundle extras = new Bundle();
         extras.putString("EXTRA_MATCH", matchNum);
         extras.putString("EXTRA_TEAM", teamNum);
