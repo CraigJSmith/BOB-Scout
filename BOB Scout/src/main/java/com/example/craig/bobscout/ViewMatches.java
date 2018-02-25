7d115054c359071008ca9f8cbbfc55fdd1c72b97
Craig Smith <craigjsmith99@gmail.com>
2018-02-24 19:42:05 -0500

    Formatting

--- BOB Scout/src/main/java/com/example/craig/bobscout/ViewMatches.java
+++ BOB Scout/src/main/java/com/example/craig/bobscout/ViewMatches.java
@@ -3,20 +3,20 @@ package com.example.craig.bobscout;
 import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Html;
 import android.widget.TextView;
 
 import java.io.File;
-import java.lang.reflect.Array;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashMap;
-import java.util.TreeMap;
+
+import static java.util.Collections.max;
 
 public class ViewMatches extends AppCompatActivity {
 
     TextView matchesView;
     String matchList;
-    HashMap<String, ArrayList<String>> matches;
+    HashMap<Integer, ArrayList<String>> matches;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,15 +24,14 @@ public class ViewMatches extends AppCompatActivity {
         setContentView(R.layout.activity_view_matches);
         matchesView = findViewById(R.id.matches);
         matchList = "";
-        matches = new HashMap<String, ArrayList<String>>();
+        matches = new HashMap<Integer, ArrayList<String>>();
 
         File dir = new File(Environment.getExternalStorageDirectory(), "/BOBScout/");
         File[] files = dir.listFiles();
 
-
         for(File f : files) {
             String[] parts = f.getName().split("[_.]");
-            String match = parts[0];
+            Integer match = Integer.parseInt(parts[0]);
             String team = parts[1];
 
             if(!matches.keySet().contains(match)) {
@@ -42,17 +41,22 @@ public class ViewMatches extends AppCompatActivity {
 
         }
 
-        for(int i = 0; i < 100; i++) {
-            matchList += "Match " + i + ": ";
+        for(int i = 1; i <= max(matches.keySet()); i++) {
+            matchList += "<b>Match " + i + ": </b>";
 
-            if(matches.keySet().contains(String.valueOf(i))) {
-                for (String team : matches.get(String.valueOf(i))) {
-                    matchList += team + ", ";
+            if(matches.keySet().contains(i)) {
+                for (String team : matches.get(i)) {
+                    matchList += team + "  ";
                 }
+
             }
-            matchList += "\n";
+            matchList += "<br></br>";
         }
 
-        matchesView.setText(matchList);
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
+            matchesView.setText(Html.fromHtml(matchList,Html.FROM_HTML_MODE_LEGACY));
+        } else {
+            matchesView.setText(Html.fromHtml(matchList));
+        }
     }
 }
